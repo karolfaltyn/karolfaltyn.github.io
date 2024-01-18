aa98d31d6aa6d6f12f808fcda6012985d2511a73
Karol Faltyn <karolfaltyn03@gmail.com>
2024-01-18 12:00:15 +0100

    created AboutMe component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Hero } from "./components/Hero";
 import { Footer } from "./parts/Footer";
-
+import { AboutMe } from "./components/AboutMe";
 export const App = () => {
   return (
     <>
       <Hero />
+      <AboutMe />
       <Footer />
     </>
   );
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,24 @@ export const AboutMe = () => {
     <>
       <section id="about" className="about">
         <div className="container">
-            .
+          <div className="AboutMe__content">
+            <div className="AboutMe__img-side">
+              {/* <img src="" alt="" /> */}
+            </div>
+            <div className="AboutMe__text-side">
+              <h3>About me</h3>
+              <h4>
+                Front-end Developer <br /> based in Wrocław, Poland.
+              </h4>
+              <p>
+                Hey, my name is Karol, and I'm a Frontend Developer. My passion
+                is to create web application for global reach and accessibility.
+                <br />
+                <br />
+                My main stack currently is React.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
