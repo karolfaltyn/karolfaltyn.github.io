d984df91b4893d39000747aa2eaf4999efd6abe2
Karol Faltyn <karolfaltyn03@gmail.com>
2024-01-29 18:09:11 +0100

    changed Resume on CV & added my bio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
-import { Resume } from "./pages/Resume";
+import { CV } from "./pages/CV";
 import { NoPage } from "./pages/NoPage";
 
 export const App = () => {
@@ -11,7 +11,7 @@ export const App = () => {
         <Routes>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="resume" element={<Resume />} />
+          <Route path="cv" element={<CV />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
