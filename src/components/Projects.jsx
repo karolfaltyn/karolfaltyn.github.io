b44dec844073c50753d2a35cad2b35267b68e680
Karol Faltyn <karolfaltyn03@gmail.com>
2024-02-06 17:02:21 +0100

    fixed issues & added some projects to Project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,9 @@ import "../assets/style/projects.css";
 
 export const Projects = () => {
   let projects = require("../assets/images/svg-on-pages/projects.svg").default;
-  let project_img = require("../assets/images/project-images/abstract.png");
+  let number_analyzer = require("../assets/images/project-images/number-analyzer.png");
+  let asteroids = require("../assets/images/project-images/asteroids.png");
+  let abstract = require("../assets/images/project-images/abstract.png");
   let code = require("../assets/images/other-icons/code-icon.svg").default;
   let link =
     require("../assets/images/other-icons/link-external-icon.svg").default;
@@ -17,20 +19,17 @@ export const Projects = () => {
               <h2>PORTFOLIO</h2>
               <p>Here are part of my projects</p>
             </div>
+
             <div className="Projects__box">
               <div className="Projects__img-side">
-                <img src={project_img} alt="project" />
+                <img src={number_analyzer} alt="project" />
               </div>
-              <hr />
               <div className="Projects__text-side">
                 <div className="Projects__desc">
                   <h3>Project Name</h3>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Adipisci quam eligendi nostrum ad vel voluptatum,
-                    consequatur quae, a voluptatibus, eius labore quisquam velit
-                    itaque ex temporibus inventore optio laudantium
-                    necessitatibus!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
+                    numquam.
                   </p>
                 </div>
                 <div className="Projects__live-code">
@@ -46,6 +45,114 @@ export const Projects = () => {
                   <a
                     aria-label="project live view"
                     href="##"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live
+                    <img src={link} alt="link" />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="Projects__box">
+              <div className="Projects__img-side">
+                <img src={number_analyzer} alt="project" />
+              </div>
+              <div className="Projects__text-side">
+                <div className="Projects__desc">
+                  <h3>Number Analyzer</h3>
+                  <p>
+                    Web application which generates numbers and categorizes them
+                    as even, odd or prime
+                  </p>
+                </div>
+                <div className="Projects__live-code">
+                  <a
+                    aria-label="project code"
+                    href="https://github.com/karolfaltyn/number-analyzer"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Code
+                    <img src={code} alt="code" />
+                  </a>
+                  <a
+                    aria-label="project live view"
+                    href="https://karolfaltyn.github.io/number-analyzer/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live
+                    <img src={link} alt="link" />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="Projects__box">
+              <div className="Projects__img-side">
+                <img src={asteroids} alt="project" />
+              </div>
+              <div className="Projects__text-side">
+                <div className="Projects__desc">
+                  <h3>Asteroids</h3>
+                  <p>
+                    A classic arcade-style game from 1979, that allows players
+                    to control a spaceship and navigate through an asteroid
+                    field, avoiding collisions and shooting asteroids.
+                  </p>
+                </div>
+                <div className="Projects__live-code">
+                  <a
+                    aria-label="project code"
+                    href="https://github.com/karolfaltyn/asteroids"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Code
+                    <img src={code} alt="code" />
+                  </a>
+                  <a
+                    aria-label="project live view"
+                    href="https://karolfaltyn.github.io/asteroids/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live
+                    <img src={link} alt="link" />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="Projects__box">
+              <div className="Projects__img-side">
+                <img src={abstract} alt="project" />
+              </div>
+              <div className="Projects__text-side">
+                <div className="Projects__desc">
+                  <h3>Abstract</h3>
+                  <p>
+                    This project is my response to a challenge from the frontend
+                    practice website. The goal is to enhance your HTML and CSS
+                    skills by building a visually appealing and responsive
+                    website.
+                  </p>
+                </div>
+                <div className="Projects__live-code">
+                  <a
+                    aria-label="project code"
+                    href="https://github.com/karolfaltyn/abstract"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Code
+                    <img src={code} alt="code" />
+                  </a>
+                  <a
+                    aria-label="project live view"
+                    href="https://karolfaltyn.github.io/abstract/"
                     target="_blank"
                     rel="noreferrer"
                   >
