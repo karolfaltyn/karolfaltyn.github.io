699e1281b331124ef9cb86c062959f11e3e680c1
Karol Faltyn <karolfaltyn03@gmail.com>
2024-02-28 17:42:54 +0100

    added ScrollToTop function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { ScrollToTop } from "./utils/ScrollToTop";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { CV } from "./pages/CV";
@@ -9,6 +10,7 @@ export const App = () => {
   return (
     <>
       <HashRouter>
+        <ScrollToTop />
         <Routes>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
