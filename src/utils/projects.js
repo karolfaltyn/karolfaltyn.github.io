01b496138321977a9491b0c67ea6c7097d4ea713
Karol Faltyn <karolfaltyn03@gmail.com>
2025-06-21 20:43:50 +0200

    reorganized project technologies

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,7 +4,7 @@ export const projectData = [
     name: "Solar Sense",
     desc: "The project enables remote real-time monitoring of photovoltaic production via the Solax Cloud API. The responsive application supports Polish and English and includes a demo mode for users without photovoltaic systems. A custom-built proxy server manages CORS headers to ensure secure API communication across browsers.",
     tech_used:
-      "React, TypeScript, Tailwind CSS, Radix UI, API Integration, i18n (Internationalization), PostCSS",
+      "React, TypeScript, Vite, ESLint, PostCSS, Tailwind CSS, Radix UI, RWD, REST API, Custom External Proxy Server (CORS bypass), i18n (Internationalization), PWA",
     link_code: "https://github.com/karolfaltyn/solar-sense/",
     link_live: "https://karolfaltyn.github.io/solar-sense/",
   },
@@ -13,7 +13,7 @@ export const projectData = [
     name: "Zen",
     desc: "A comprehensive management application designed to help you organize tasks, take notes, and manage your time efficiently. It offers a modern, responsive, and intuitive user experience. Additionally, all data is stored in the browser's local storage, ensuring your information is saved securely between sessions.",
     tech_used:
-      "React, TypeScript, Tailwind CSS, Shadcn UI, PostCSS, Local Storage",
+      "React, TypeScript, Vite, ESLint, PostCSS, Tailwind CSS, Shadcn UI, RWD, Local Storage, PWA",
     link_code: "https://github.com/karolfaltyn/zen/",
     link_live: "https://karolfaltyn.github.io/zen/",
   },
@@ -21,7 +21,7 @@ export const projectData = [
     link: require("../assets/images/project-images/coffeehouse.png"),
     name: "Coffeehouse",
     desc: "This coffee website offers a user-friendly shopping cart where customers can add, remove, and adjust items with ease. Session storage ensures that cart data is retained throughout the browsing session, providing a smooth and convenient shopping experience.",
-    tech_used: "React, JavaScript, Tailwind CSS, PostCSS, Session Storage",
+    tech_used: "React, JavaScript, PostCSS, Tailwind CSS, RWD, Session Storage, PWA",
     link_code: "https://github.com/karolfaltyn/coffeehouse/",
     link_live: "https://karolfaltyn.github.io/coffeehouse/",
   },
@@ -30,7 +30,7 @@ export const projectData = [
     name: "Weather",
     desc: "This app uses an API to display weather data. It shows real-time weather information and forecasts based on either the entered location or the device's geographic location, utilizing the Geolocation API. Weather data is retrieved from the OpenWeatherMap API.",
     tech_used:
-      "React, JavaScript, Tailwind CSS, Material UI, API Integration, PostCSS",
+      "React, JavaScript, PostCSS, Tailwind CSS, Material UI, RWD, REST API, PWA",
     link_code: "https://github.com/karolfaltyn/weather/",
     link_live: "https://karolfaltyn.github.io/weather/",
   },
